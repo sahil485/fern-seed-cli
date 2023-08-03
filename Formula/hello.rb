91fb8d40ef605558199fa6dc12898c39bdd3eee9
sahil485 <sahilchatiwala@gmail.com>
2023-08-03 02:41:22 -0400

    fixed errors in brew

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,8 +1,8 @@
 class Hello < Formula
     desc "Fern Seed CLI Skeleton"
     homepage "https://github.com/sahil485/homebrew-seed"
-    url "https://github.com/sahil485/homebrew-seed/releases/tag/v1.0.0"
-    sha256 "6c1870db6a831ad868a327f7fcc6418c54de6c8cfa4577a217ba72ea386b37fe"
+    url "https://github.com/sahil485/homebrew-seed/releases/download/v1.0.0/seed-macos-x64.tar.gz"
+    sha256 "f2316d682586264f36e638afe17ceeeccef1771e3fcee19b7c53d5a53c896a7e"
     version "1.0.0"
     def install
       bin.install "seed"
