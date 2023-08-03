0b9f6b8b3d687acc34eaf4f0f502a404abca9e03
sahil485 <sahilchatiwala@gmail.com>
2023-08-03 02:51:22 -0400

    trying yet again

--- Formula/seed.rb
+++ Formula/seed.rb
@@ -2,7 +2,7 @@ class Seed < Formula
     desc "Fern Seed CLI Skeleton"
     homepage "https://github.com/sahil485/homebrew-seed"
     url "https://github.com/sahil485/homebrew-seed/releases/download/v1.0.0/seed-macos-x64.tar.gz"
-    sha256 "f2316d682586264f36e638afe17ceeeccef1771e3fcee19b7c53d5a53c896a7e"
+    sha256 "0ce3362e266747fe277e3d9777bd0f5fb269ed262c09727ab7713dbf1881b759"
     version "1.0.0"
     def install
       bin.install "seed"
