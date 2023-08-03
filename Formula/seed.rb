c14e79782481631105eea36ff954ef163ac2c464
sahil485 <sahilchatiwala@gmail.com>
2023-08-03 02:53:37 -0400

    SHA mismatch

--- Formula/seed.rb
+++ Formula/seed.rb
@@ -2,7 +2,7 @@ class Seed < Formula
     desc "Fern Seed CLI Skeleton"
     homepage "https://github.com/sahil485/homebrew-seed"
     url "https://github.com/sahil485/homebrew-seed/releases/download/v1.0.0/seed-macos-x64.tar.gz"
-    sha256 "0ce3362e266747fe277e3d9777bd0f5fb269ed262c09727ab7713dbf1881b759"
+    sha256 "6c1870db6a831ad868a327f7fcc6418c54de6c8cfa4577a217ba72ea386b37fe"
     version "1.0.0"
     def install
       bin.install "seed"
